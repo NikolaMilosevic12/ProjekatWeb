40c7a269b9959378e4379927c550b051d274675f
Nikola Milošević <nikolamilosevic1387@gmail.com>
2026-03-18 20:05:10 +0100

    Valkjda radi

--- NikolaMilosevic/konekcija.cs
+++ NikolaMilosevic/konekcija.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -26,5 +28,28 @@ namespace NikolaMilosevic
         {
             if (veza.State == System.Data.ConnectionState.Open) veza.Close();
         }
+
+        public int ProveraKorisnika(string email, string lozinka)
+        {
+            SqlConnection veza = OtvoriKonekciju();
+
+            SqlCommand command = new SqlCommand("provera_korisnika", veza);
+            command.CommandType = CommandType.StoredProcedure;
+
+            command.Parameters.AddWithValue("@email", email);
+            command.Parameters.AddWithValue("@lozinka", lozinka);
+
+            SqlParameter returnValue = new SqlParameter();
+            returnValue.Direction = ParameterDirection.ReturnValue;
+            command.Parameters.Add(returnValue);
+
+            command.ExecuteNonQuery();
+
+            ZatvoriKonekciju();
+
+            return (int)returnValue.Value;
+        }
+
+
     }
 }
