65024d12b9a83995bac744ec8d9d4985e2dd6369
nikol <nikol@NIKOLA>
2026-03-18 18:17:49 +0100

    Add project files.

---
+++ NikolaMilosevic/konekcija.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace NikolaMilosevic
+{
+    public class konekcija
+    {
+        private SqlConnection veza;
+
+        public konekcija()
+        {
+            veza = new SqlConnection(ConfigurationManager.ConnectionStrings["veza"].ConnectionString);
+        }
+
+        public SqlConnection OtvoriKonekciju()
+        {
+            if (veza.State == System.Data.ConnectionState.Closed) veza.Open();
+            return veza;
+        }
+
+        public void ZatvoriKonekciju()
+        {
+            if (veza.State == System.Data.ConnectionState.Open) veza.Close();
+        }
+    }
+}
